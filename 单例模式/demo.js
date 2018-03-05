05c915de64f7e60a8026fd7d7a036d611b7405b0
miracles <miracles@fanfandata.com>
2018-03-06 00:20:11 +0800

    实现简单的单例模式

--- 单例模式/demo.js
+++ 单例模式/demo.js
@@ -5,4 +5,45 @@
 
 var Singleton = function (name) {
   this.name = name
+  this.instance = null
 }
+
+Singleton.prototype.getName = function () {
+  console.log(this.name)
+}
+
+Singleton.getInstance = function (name) {
+  if (!this.instance) {
+    this.instance = new Singleton(name)
+  } 
+  return this.instance
+}
+
+var a = Singleton.getInstance('s1')
+var b = Singleton.getInstance('s2')
+
+console.log(a === b)
+
+
+var Singleton2 = function (name) {
+  this.name = name
+}
+
+Singleton2.prototype.getName = function () {
+  console.log(this.name)
+}
+
+Singleton2.getInstance = (function () {
+  var instance = null
+  return function (name) {
+    if (!instance) {
+      instance = new Singleton2(name)
+    }
+    return instance
+  }
+})()
+
+var c = Singleton2.getInstance('s1')
+var d = Singleton2.getInstance('s2')
+
+console.log(c === d)
